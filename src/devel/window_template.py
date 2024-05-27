e1aa7b363e0ca53fc6aa7d4d7e25bb9640cc06c8
Night-Traders-Dev <quegmeister@gmail.com>
2024-05-27 06:35:29 -0400

    Devel: window_template extending to notifications, help text, dialogs

--- src/devel/window_template.py
+++ src/devel/window_template.py
@@ -1,25 +1,34 @@
 from textual.app import App, ComposeResult
-from textual.containers import Container
+from textual.containers import Container, Vertical
 from textual.events import Click
 from textual.geometry import Offset
 from textual.widget import Widget
-from textual.widgets import Placeholder, Static
+from textual.widgets import Static
+import asyncio
 
 class BarContainer(Container):
     """Topbar Container"""
 
+    async def on_ready(self) -> None:
+        self.set_interval(5, self.parent.remove())
+
     def on_click(self, event: Click) -> None:
         self.parent.remove()
 
-class Window(Placeholder):
+class TitleText(Static):
+    """TitleText Widget"""
+
+class Window(Vertical):
 
     DEFAULT_CSS = """
     Window {
         overlay: screen;
         dock: left;
         layout: vertical;
+        content-align: center middle;
+        background: rgba(119, 41, 83, 0.50);
     }
-    #title-bar {
+     Window #title-bar {
         width: 100%;
         height: 1;
         background: rgba(51, 51, 51, 0.50);
@@ -27,6 +36,10 @@ class Window(Placeholder):
         layout: horizontal;
     }
 
+    Window #title  {
+        dock: top;
+
+    }
     """
 
     def __init__(self, x: int, y: int, width: int, height: int, label: str, title: str, id: str | None = None) -> None:
@@ -42,9 +55,14 @@ class Window(Placeholder):
     def render(self) -> str:
         return self.label
 
-    def compose(self) -> ComposeResult:
-        with BarContainer(id="title-bar"):
-            yield Static(f" [bold]{self.title}[/bold]", id="title")
+    def set_title(self, new_title: str):
+
+        title_bar = self.query_one("#title-bar")
+        title_text = title_bar.query_one("#title")
+
+        old_text = title_text.renderable.plain
+
+        title_text.update(f" [bold]{new_title}[/bold]")
 
     async def _on_click(self, event: Click) -> None:
         if isinstance(self.parent, Widget):
@@ -52,11 +70,14 @@ class Window(Placeholder):
             self.parent.move_child(self, after=-1)
             return await super()._on_click(event)
 
+    def compose(self) -> ComposeResult:
+        with BarContainer(id="title-bar"):
+            yield TitleText(f" [bold]{self.title}[/bold]", id="title")
+
 class WindowTemplate(App[None]):
     def compose(self) -> ComposeResult:
-        yield Window(3, 3, 20, 10, f"some emojis", "Emojis", "emoji")
-        yield Window(4, 7, 20, 5, f"A random dialog", "Dialog", "dialog")
-        yield Window(7, 10, 20, 5, f"Some cool notification", "Notification", "notification") 
+        x, y = self.size
+        yield Window((x - 22), (y - 6), 20, 5, f"\nSome cool notification", "Notification", "notification") 
 
 if __name__ == "__main__":
     WindowTemplate().run()
