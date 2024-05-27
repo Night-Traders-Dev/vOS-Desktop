debecbe1b33faaa9bc8a9445e0172fa7bd59376e
Night-Traders-Dev <quegmeister@gmail.com>
2024-05-27 08:22:16 -0400

    Devel: seperated notif_template from window_template

--- src/devel/notif_template.py
+++ src/devel/notif_template.py
@@ -9,22 +9,20 @@ import asyncio
 
 class BarContainer(Container):
     """Topbar Container"""
-#    time = reactive(0)
+    time = reactive(0)
 
-#    def update_timer(self) -> None:
-#        self.time += 1
+    def update_timer(self) -> None:
+        self.time += 1
 
-#    def watch_time(self, time: int) -> None:
-#        if time == 3:
-#            self.timer.pause()
-#            self.time = 0
-#            self.parent.remove()
+    def watch_time(self, time: int) -> None:
+        if time == 3:
+            self.timer.pause()
+            self.time = 0
+            self.parent.remove()
 
-#    def on_mount(self) -> None:
-#        self.timer = self.set_interval(1, self.update_timer)
+    def on_mount(self) -> None:
+        self.timer = self.set_interval(1, self.update_timer)
 
-    def on_click(self, event: Click) -> None:
-        self.parent.remove()
 
 class TitleText(Static):
     """TitleText Widget"""
@@ -76,10 +74,7 @@ class Window(Vertical):
         title_text.update(f" [bold]{new_title}[/bold]")
 
     async def _on_click(self, event: Click) -> None:
-        if isinstance(self.parent, Widget):
-            self.focus()
-            self.parent.move_child(self, after=-1)
-            return await super()._on_click(event)
+        self.remove()
 
     def compose(self) -> ComposeResult:
         with BarContainer(id="title-bar"):
