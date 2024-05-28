4b1a8d19c36bf2d27f490f1cb431941c1aad87b9
Night-Traders-Dev <quegmeister@gmail.com>
2024-05-28 08:15:17 -0400

    Devel: event_template now grabs messages and displays current datetime

--- src/devel/event_template.py
+++ src/devel/event_template.py
@@ -1,11 +1,27 @@
 from textual import on
 from textual.app import App, ComposeResult
+from textual.containers import Vertical, Horizontal
 from textual.css.query import NoMatches
 from textual.events import *
+from textual.message import *
 from textual.widgets import Log
+from datetime import datetime
 
 class EventLog(App[None]):
-    # Define the set of event types to capture
+
+    DEFAULT_CSS = """
+        App {
+            align: left top;
+        }
+        Log {
+        content-align: left top;
+        layout: vertical;
+        text-align: left;
+        background: rgba(119, 41, 83, 100.0);
+        height: 100%;
+        width: 100%;
+        }
+        """
     CAPTURED_EVENTS = {
              Resize, MouseDown,
              MouseUp, Click,
@@ -16,13 +32,20 @@ class EventLog(App[None]):
 
     def compose(self) -> ComposeResult:
         yield Log()
+#        self.debug.box_sizing = "content-box"
 
+    @on(Message)
+    def log_message(self, message: Message) -> None:
+        if f"{message!r}" != "Idle()":
+            try:
+                self.query_one(Log).write_line(f"{datetime.now()} Message: {message!r}\n")
+            except NoMatches:
+                pass
     @on(Event)
     def log_event(self, event: Event) -> None:
-        # Check if the event type is in the whitelist
         if type(event) in self.CAPTURED_EVENTS:
             try:
-                self.query_one(Log).write_line(f"{event!r}")
+                self.query_one(Log).write_line(f"{datetime.now()} Event: {event!r}\n")
             except NoMatches:
                 pass
 
