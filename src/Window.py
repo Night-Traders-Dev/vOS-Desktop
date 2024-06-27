2b8e6c1c108e83aeced41edf5e4cf1b3fc9b5595
Night-Traders-Dev <quegmeister@gmail.com>
2024-06-27 14:02:35 -0400

    Window: simplify id and title

--- src/Window.py
+++ src/Window.py
@@ -1,16 +1,20 @@
 from textual.app import App
 from components.dashboard import Window
 
+
+id = "dashboard_window"
+title = "vOS Dashboard"
+
 class Dashboard(App[None]):
     def on_ready(self) -> None:
         try:
-            window = self.query_one("#dashboard_window")
+            window = self.query_one(f"#[id]")
             if window:
                 return
         except Exception as e:
             pass
 
-        self.mount(Window(0, 0, 50, 20, "", "vOS Dashboard", "dashboard_window"))
+        self.mount(Window(0, 0, 50, 20, "", title, id))
 
 if __name__ == "__main__":
     Dashboard().run()
