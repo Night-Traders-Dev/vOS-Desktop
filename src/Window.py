e768b4b7fb9a142e1623489594e38ab0a70ab235
Night-Traders-Dev <quegmeister@gmail.com>
2024-06-29 09:05:28 -0400

    Window: updated syntax

--- src/Window.py
+++ src/Window.py
@@ -1,20 +1,12 @@
 from textual.app import App
 from components.windows import Window
 
-
 id = "test_window"
 title = "Test Window"
 label = "Hello World!"
 
 class TestWindow(App[None]):
     def on_ready(self) -> None:
-        try:
-            window = self.query_one(f"#[id]")
-            if window:
-                return
-        except Exception as e:
-            pass
-
         self.mount(
             Window(
                 x=0,
@@ -24,7 +16,8 @@ class TestWindow(App[None]):
                 label=label,
                 title=title,
                 id=id
-        ))
+            )
+        )
 
 if __name__ == "__main__":
     TestWindow().run()
